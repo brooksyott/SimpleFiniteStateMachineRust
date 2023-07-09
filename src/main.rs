0639c44faced8afa43596ab2aa00805aadd74c96
Dave Brooks <david.brooks@live.ca>
2023-07-09 08:52:39 -0400

    Minor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use crate::simple_finite_state_machine::finite_state_machine::FiniteStateMachine;
-mod simple_finite_state_machine;
+// use crate::simple_finite_state_machine::finite_state_machine::FiniteStateMachine;
 mod phone_call;
+mod simple_finite_state_machine;
 use crate::phone_call::build_phone;
-use std::fmt::Display;
-use enum_display::EnumDisplay;
 
 fn main() {
     build_phone::do_phone_call_1();
